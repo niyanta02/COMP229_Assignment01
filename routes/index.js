e83fae75e2b131093137521ab4ed5e05ea201dc4
niyanta02 <niyanta.official@gmail.com>
2022-02-11 21:31:40 -0500

    Week 1 Assignment

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET Home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET Project page. */
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'Project Page' });
+});
+
+/* GET Service page. */
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Service' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Page' });
+});
+
+
+module.exports = router;
